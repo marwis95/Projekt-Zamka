1bceb9b42cb5d22278668034104820e21669372a
marwis95 <marwis95@o2.pl>
2020-04-14 19:25:49 +0200

    Init

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c	ATmega32	F_CPU = 8000000 Hz
+ * 
+ * Created on: 2020-04-14
+ *     Author: admin
+*/
+// do³¹czanie systemowych plików nag³ówkowych
+#include <avr/io.h>
+#include <util/delay.h>
+
+// miejsce na do³¹czanie w³asnych plików
+
+// g³ówna funkcja programu - main()
+int main( void ) {
+
+// sekcja inicjalizacji peryferiów
+
+// g³ówna pêtla programu
+	while(1) {
+
+	}
+
+}
