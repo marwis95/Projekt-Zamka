5b922bf7404a218c89d4bde8d094f87e6229129c
marwis95 <marwis95@o2.pl>
2020-04-27 21:04:36 +0200

    Increment decrement press

--- main.c
+++ main.c
@@ -1,97 +1,79 @@
-    #include <stdio.h>
+        #include <stdio.h>
+#include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include "hd44780.h"
-
 #include "uart.h"
+
 #define KEY (1<<PC0)
 
+uint16_t key_lock;
+unsigned int encoder = 0;
+
 //INT0 interrupt
-ISR(INT0_vect )
-{
-    if(!bit_is_clear(PIND, PD3))
-    {
-            LCD_Clear();
-    LCD_GoTo(0,0);
-    LCD_WriteText("+");
-    }
-    else
-    {
-            LCD_Clear();
-    LCD_GoTo(0,0);
-    LCD_WriteText("-");
+ISR( INT0_vect ) {
+    if ( !bit_is_clear( PIND, PD3 ) ) {
+        encoder += 1;
+    } else {
+        encoder -= 1;
     }
 }
 
 //INT1 interrupt
-ISR(INT1_vect )
-{
-    if(!bit_is_clear(PIND, PD2))
-    {
-            LCD_Clear();
-    LCD_GoTo(0,0);
-    LCD_WriteText("+");
-    }
-    else
-    {
-           LCD_Clear();
-    LCD_GoTo(0,0);
-    LCD_WriteText("-");
+ISR( INT1_vect ) {
+    if ( !bit_is_clear( PIND, PD2 ) ) {
+        encoder += 1;
+    } else {
+        encoder -= 1;
     }
 }
 
-int main(void)
-{
+int main( void ) {
+
+    char bufor [4];
 
     PORTC |= KEY;
 
-    DDRD &=~ (1 << PD2);
-    DDRD &=~ (1 << PD3);
-    PORTD |= (1 << PD3)|(1 << PD2);
+    DDRD &= ~( 1 << PD2 );
+    DDRD &= ~( 1 << PD3 );
+    PORTD |= ( 1 << PD3 ) | ( 1 << PD2 );
 
-    GICR |= (1<<INT0)|(1<<INT1);
-    MCUCR |= (1<<ISC01)|(1<<ISC11)|(1<<ISC10);
+    GICR |= ( 1 << INT0 ) | ( 1 << INT1 );
+    MCUCR |= ( 1 << ISC01 ) | ( 1 << ISC11 ) | ( 1 << ISC10 );
 
     sei();
 
     LCD_Initalize();
     LCD_Home();
     LCD_Clear();
-    LCD_GoTo(0,0);
-    LCD_WriteText("INIT");
+    LCD_GoTo( 0, 0 );
 
+    _delay_ms( 10 );
 
+    while ( 1 ) {
+          if( !key_lock && !(PINC & KEY ) ) {
+           key_lock = 50000;
+           LCD_GoTo( 0, 1 );
+           LCD_WriteText( "PRESS" );
+          } else if( key_lock && (PINC & KEY ) ) key_lock++;
 
-   _delay_ms(10);
-   while(1)
-   {
-   if(!(PINC & KEY ))  {
-    LCD_GoTo(0,1);
-    LCD_WriteText("PRESS");
-   }
-    _delay_ms(1);
-   }
+        LCD_GoTo( 0, 0 );
+        itoa(encoder,bufor,10);
+        if(encoder < 10){
+            bufor[1] = bufor[0];
+            bufor[0] = '0';
+            bufor[2] = ' ';
+            bufor[3] = ' ';
+        }
 
-  return 0;
+        LCD_WriteText( bufor );
+
+        LCD_GoTo( 2, 0 );
+        LCD_WriteText("               ");
+        _delay_ms( 1 );
+    }
+
+    return 0;
 }
-
-
-/*
-#include <avr/io.h>
-#include "hd44780.h"
-#include <stdio.h>
-#include <util/delay.h>
-
-int main(void){
-    LCD_Initalize();
-    LCD_Home();
-    LCD_Clear();
-    LCD_GoTo(0,0);
-    LCD_WriteText("test");
-    LCD_GoTo(0,1);
-    LCD_WriteText("xD");
-    for(;;);
-}
-*/
--- mk_build.c
+++ mk_build.c
@@ -24,7 +24,7 @@
 /* Do not any changes in next four lines */
 /* These lines will be updated automaticaly by MkClipse */
 const char MkBuildVersion[] PROGMEM = {"1.0.0"};
-const char MkBuildNumber[] PROGMEM = {"14"};
+const char MkBuildNumber[] PROGMEM = {"49"};
 const char MkBuildDate[] PROGMEM = {"2020-04-27"};
-const char MkBuildTime[] PROGMEM = {"19:32:15"};
+const char MkBuildTime[] PROGMEM = {"21:03:08"};
 
